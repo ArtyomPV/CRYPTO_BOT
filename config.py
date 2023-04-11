12a42815319c406d355caf4062b90b3265825472
Artyom <itcoding76@gmail.com>
2023-04-11 11:56:15 +0200

    Crypto Bot

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '6293738072:AAGYvjTq1ffXdunvz4XFSulXFnFYYpI5Gd4'
+API_Key = 'b559455eab15edb3be830755'
+keys = {
+    'биткоин': 'BTC',
+    'эфориум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'футы': 'GBP'
+}
