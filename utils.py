3fc3ee7d63156d47d4998567c8c1282bcf02fe00
Artyom <itcoding76@gmail.com>
2023-04-11 11:55:16 +0200

    Crypto Bot

---
+++ utils.py
@@ -0,0 +1,36 @@
+import json
+import requests
+
+from config import keys, API_Key
+
+
+class APIException(Exception):
+    pass
+
+
+class Converter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            qoute_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}.')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}.')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/{API_Key}/pair/{qoute_ticker}/{base_ticker}')
+        resp = json.loads(r.content)
+        price = resp['conversion_rate'] * amount
+        price = round(price, 2)
+        return price
